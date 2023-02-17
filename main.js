f33da5e5236bf42fda195b2780803ec5c8b81923
Kevin Bernard <kevinbernard@Kevins-MacBook-Pro.local>
2023-02-17 13:37:13 +0100

    chrono pas fini, block lienar

--- main.js
+++ main.js
@@ -41,7 +41,14 @@ setInterval(() => {
     }
 })
 
-    
+    var timer_counter = 0;
+    var how_quick = 90
+
+    document.getElementById("t").innerHTML = timer_counter;
+
+    console.log(timer_counter)
+
+    setInterval(time , how_quick)
 
 function time(event){
     if (timer_counter != 99999) {
