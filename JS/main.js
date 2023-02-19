4d58af6c468a15ca7fc4f35650dc64dd5c6053c5
Kevin Bernard <kevinbernard@Kevins-MacBook-Pro.local>
2023-02-19 17:54:07 +0100

    jeu a 95% fini

--- JS/main.js
+++ JS/main.js
@@ -1,72 +1,109 @@
+let startchrono = false;
+var timer_counter = 0;
+var refresh_time = 1000
+
 const cactus = document.querySelector('#cactus');
+const helico = document.querySelector('#helico');
+const background = document.querySelector('#background');
 
-document.addEventListener('keydown', event => {
-    move();
-    // chrono();
-});
+function moveBlock() {
+    if (!background.classList.contains('backgroundAnimation')){
+        background.classList.add('backgroundAnimation');
+    }
 
-function move() {
     if (!cactus.classList.contains('move')) {
         cactus.classList.add('move');
     }
+
+    if (!helico.classList.contains('move')) {
+        helico.classList.add('move');
+    }
 }
+
 
 const dino = document.querySelector('#dino');
 
-document.addEventListener('keydown', event => {
-    jump(event);
-    time(event);
+document.addEventListener('keyup', event => {
+    dinoJump(event);
+    //time(event);
 });
 
-function jump(event) {
+document.addEventListener('keydown', event => {
+    if (!startchrono){
+        startchrono= true;
+    }
+    console.log("DOWN DETECTED");    
+    moveBlock(); 
+    dinoDown(event);
+   // time();
+});
+
+function dinoJump(event) {
     if (!(event.code === 'Space')) {
         return;
     }
-    if (!dino.classList.contains('jump')) {
+    if (!dino.classList.contains('jump') && !dino.classList.contains('down')) {
+        document.getElementById('dino').setAttribute('src','/img/Jump.png');
+        dino.classList.remove('down');
         dino.classList.add('jump');
-        setTimeout(function() {dino.classList.remove('jump')}, 2000);
+        setTimeout(function() {dino.classList.remove('jump');document.getElementById('dino').setAttribute('src','/img/Run.png');}, 2000);
     }
 }
 
-function down(event) {
-    if (!(event.code === 's')) {
-        return;
+function dinoDown(event) {
+    if (!(event.code === 'KeyS')) {
+         return;
     }
-    if (!dino.classList.contains('down')) {
+    if (!dino.classList.contains('down') && !dino.classList.contains('jump')) {
+        document.getElementById('dino').setAttribute('src','/img/Dirft.png');
+        dino.classList.remove('jump');
         dino.classList.add('down');
-        setTimeout(function() {dino.classList.remove('down')}, 2000)
+        setTimeout(function() {dino.classList.remove('down');document.getElementById('dino').setAttribute('src','/img/Run.png');}, 2000)
     }
 }
 
 function isCrashed() {
     let dinoPosition = dino.getBoundingClientRect();
     let cactusPosition = cactus.getBoundingClientRect();
-
-    return dinoPosition.right > cactusPosition.left && dinoPosition.left < cactusPosition.right && dinoPosition.bottom > cactusPosition.top;
+    let helicoPosition = helico.getBoundingClientRect();
+    return (dinoPosition.right > cactusPosition.left 
+    && dinoPosition.left < cactusPosition.right 
+    && dinoPosition.bottom > cactusPosition.top) || (
+        dinoPosition.right > helicoPosition.left 
+    && dinoPosition.left < helicoPosition.right 
+    && dinoPosition.top > helicoPosition.bottom
+    ) ;
 }
 
-setInterval(() => {
-    if (isCrashed()) {
-        alert('Game Over!');
+document.getElementById("t").innerHTML = timer_counter;
+
+console.log(timer_counter)
+
+function updateTimer(){
+    if (startchrono){
+        if (!background.classList.contains('backgroundAnimation')){
+            background.classList.add('backgroundAnimation');
+        }
+        timer_counter++;
+        document.getElementById('t').innerHTML = timer_counter ;
+
+        if (timer_counter >= 30){
+            startchrono = false;
+            alert('WIN !');
+            background.classList.remove('backgroundAnimation');
+            history.back();
+
+            // gerer le prochain niveau
+        }
+        else if (isCrashed()) {
+            alert('Game Over!');
+            timer_counter = 0;
+            document.getElementById('t').innerHTML = 0 ;
+            background.classList.remove('backgroundAnimation');
+            history.back();
+            
+        }
     }
-})
+}
 
-//     var timer_counter = 0;
-//     var how_quick = 90
-
-//     document.getElementById("t").innerHTML = timer_counter;
-
-//     console.log(timer_counter)
-
-//     setInterval(time , how_quick)
-
-// function time(event){
-//     if (timer_counter != 99999) {
-//         timer_counter++;
-//         document.getElementById("t").innerHTML = timer_counter;
-//         console.log(timer_counter);
-// } else {
-//     timer_counter = 0;
-//     time()
-// }
-// }
+setInterval(updateTimer, refresh_time)
