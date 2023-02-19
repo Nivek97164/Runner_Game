dcbbceec05c86faf3d84cbed6633c635809ba70d
Kevin Bernard <kevinbernard@Kevins-MacBook-Pro.local>
2023-02-19 15:44:20 +0100

    background, ..

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,7 @@ const cactus = document.querySelector('#cactus');
 
 document.addEventListener('keydown', event => {
     move();
-    chrono();
+    // chrono();
 });
 
 function move() {
@@ -15,7 +15,7 @@ const dino = document.querySelector('#dino');
 
 document.addEventListener('keydown', event => {
     jump(event);
-    time(event)
+    time(event);
 });
 
 function jump(event) {
@@ -25,6 +25,16 @@ function jump(event) {
     if (!dino.classList.contains('jump')) {
         dino.classList.add('jump');
         setTimeout(function() {dino.classList.remove('jump')}, 2000);
+    }
+}
+
+function down(event) {
+    if (!(event.code === 's')) {
+        return;
+    }
+    if (!dino.classList.contains('down')) {
+        dino.classList.add('down');
+        setTimeout(function() {dino.classList.remove('down')}, 2000)
     }
 }
 
@@ -41,22 +51,22 @@ setInterval(() => {
     }
 })
 
-    var timer_counter = 0;
-    var how_quick = 90
+//     var timer_counter = 0;
+//     var how_quick = 90
 
-    document.getElementById("t").innerHTML = timer_counter;
+//     document.getElementById("t").innerHTML = timer_counter;
 
-    console.log(timer_counter)
+//     console.log(timer_counter)
 
-    setInterval(time , how_quick)
+//     setInterval(time , how_quick)
 
-function time(event){
-    if (timer_counter != 99999) {
-        timer_counter++;
-        document.getElementById("t").innerHTML = timer_counter;
-        console.log(timer_counter);
-} else {
-    timer_counter = 0;
-    time()
-}
-}
+// function time(event){
+//     if (timer_counter != 99999) {
+//         timer_counter++;
+//         document.getElementById("t").innerHTML = timer_counter;
+//         console.log(timer_counter);
+// } else {
+//     timer_counter = 0;
+//     time()
+// }
+// }
